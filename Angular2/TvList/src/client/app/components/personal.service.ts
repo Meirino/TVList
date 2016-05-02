fb59199a779c1fed4a2c6b45257745529d867cbc
Meirino <jjmeiras@gmail.com>
2016-05-02 19:28:16 +0200

    Configurado WebStorm con Git

---
+++ Angular2/TvList/src/client/app/components/personal.service.ts
@@ -0,0 +1,65 @@
+import {Injectable} from 'angular2/core';
+import {withObserver} from './utils'
+
+export class Personal {
+
+  constructor(public id: number, public nombre: string,
+    public descripcion:string,
+    public rutaIMG:string,
+    public obras:string[]) {}
+}
+
+@Injectable()
+export class personalService {
+  private Personales = [
+    new Personal(1, 'Ryan Gosling','Actor protagonista en Drive','#',['Drive 2011']),
+    new Personal(2, 'Bob Odenkirk', 'Actor en Break Bad como Saul Goodman', '#', ['Breaking Bad'])
+  ]
+
+  addPersonal(persona:Personal) {
+    this.Personales.push(persona)
+  }
+
+  getLongitud() {
+    return this.Personales.length;
+  }
+
+  getCero () {
+    return 0;
+  }
+
+  getPersonales() {
+    return withObserver(this.Personales);
+  }
+
+  getPersonalbyID(id:number | string) {
+    let elem = this.Personales.filter(h => h.id === +id)[0]
+    return withObserver(new Personal(elem.id, elem.nombre, elem.descripcion, elem.rutaIMG, elem.obras));
+  }
+
+  getPersonalbyName(nom:string) {
+    let elem = this.Personales.filter(h => h.nombre == nom)
+    return elem;
+  }
+
+  getPersonalByObra(obra: string) {
+    let elem = this.Personales.filter(h => h.obras.indexOf(obra) > -1)
+    return elem;
+  }
+
+  removePersona(persona: Personal){
+    for(let i=0; i<this.Personales.length; i++){
+        if(this.Personales[i].id === persona.id){
+          this.Personales.splice(i,1);
+          break;
+        }
+    }
+    return withObserver(undefined);
+  }
+
+  savePersona(persona: Personal){
+    persona.id = this.Personales.length+1;
+    this.Personales.push(persona);
+    return withObserver(persona);
+  }
+}
---
+++ Angular2/TvList/src/client/app/components/series.service.ts
@@ -0,0 +1,76 @@
+import {Injectable} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
+import {withObserver} from './utils';
+
+export class Serie {
+  constructor(public id:number, public titulo: string,
+    public sinopsis:string, public esSerie:boolean,
+    public temporadas:number, public capitulos:number,
+    public rutaIMG:string, public categorias:String[], public personal:String[]) {}
+}
+
+  @Injectable()
+  export class seriesService {
+    private series = [
+  	   new Serie(1, 'Drive', 'Sinopsis', false, 0, 0, 'aaa', ['Drama', 'Conducción'], ['Ryan Gosling']),
+       new Serie(2, 'Breaking Bad', 'Sinopsis', true, 7, 50, 'aaa', ['Drama', 'Thriller', 'Policiaco'], ['Brian Carston'])
+    ];
+
+  addSerie(serie:Serie) {
+    this.series.push(serie);
+  }
+
+  getSeries() {
+    return withObserver(this.series);
+  }
+
+  getSeriebyID(id:number | string) {
+    let elem = this.series.filter(h => h.id === +id)[0]
+    return withObserver(new Serie(elem.id, elem.titulo, elem.sinopsis, elem.esSerie, elem.temporadas, elem.capitulos, elem.rutaIMG, elem.categorias, elem.personal));
+  }
+
+  getSeriesbyCategorias(cat:string) {
+    let elem = this.series.filter(h => h.categorias.indexOf(cat) > -1)
+    let elem2 = elem.filter(h => h.esSerie == true);
+    return elem2;
+  }
+
+  getPeliculasbyCategorias(cat:string) {
+    let elem = this.series.filter(h => h.categorias.indexOf(cat) > -1)
+    let elem2 = elem.filter(h => h.esSerie == false);
+    return elem2;
+  }
+
+  getSeriesbyTipo(tipo:boolean) {
+    let elem = this.series.filter(h => h.esSerie == tipo);
+    return elem;
+  }
+
+  removeSerie(serie: Serie){
+    for(let i=0; i<this.series.length; i++){
+        if(this.series[i].id === serie.id){
+          this.series.splice(i,1);
+          break;
+        }
+    }
+    return withObserver(undefined);
+  }
+
+  saveSerie(serie: Serie){
+    if(serie.id){
+      let oldSerie = this.series.filter(h => h.id === serie.id)[0];
+      oldSerie.titulo = serie.titulo;
+      oldSerie.sinopsis = serie.sinopsis;
+      oldSerie.esSerie = serie.esSerie;
+      oldSerie.temporadas = serie.temporadas;
+      oldSerie.capitulos = serie.capitulos;
+      oldSerie.rutaIMG = serie.rutaIMG;
+      oldSerie.categorias = serie.categorias;
+      oldSerie.personal = serie.personal;
+    } else {
+      serie.id = this.series.length+1;
+      this.series.push(serie);
+    }
+    return withObserver(serie);
+  }
+}
