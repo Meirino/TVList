4d16f596142b88f4af0c023b7b0af070ac7d5d0f
david <drimer1994@gmail.com>
2016-05-23 13:51:06 +0200

    Configurado servicio actores en component actoresListComponent.

--- Angular2/TvList/src/client/app/components/ActorList/ActoresList.component.ts
+++ Angular2/TvList/src/client/app/components/ActorList/ActoresList.component.ts
@@ -1,6 +1,4 @@
-/**
- * Created by Javi on 18/05/2016.
- */
+
 import {Component}  from 'angular2/core';
 import {RouteConfig, Router, RouterOutlet, RouteParams,CanActivate,ComponentInstruction} from 'angular2/router';
 import {Actor, ActoresService} from "../actores.service";
@@ -8,15 +6,14 @@ import {ActorDetalleComponent} from "../ActorDetalle/ActorDetalle.component";
 
 @Component({
     selector: 'ActoresList',
-    templateUrl: './app/components/ActorList/ActoresList.template.html'
+    templateUrl: './app/components/ActorList/ActoresList.template.html',
+    providers:[ActoresService]
 })
 
-export class ActoresListComponent {
-    public lista:Actor[] = [
-        new Actor(0, 'Ryan Gosling', 'Protagonista de la pelicula Drive', 'http://www.soletopia.com/wp-content/uploads/2013/07/ryan-gosling-only-god-forgives-style-11-navy-vest.jpg', ['Drive'])
-    ];
-    public service: ActoresService;
+export class ActoresListComponent{
+    public lista:Actor[]=this._actoresServicio.getDatos();
 
-    constructor(public router:Router) {
+    constructor(public router:Router, private _actoresServicio:ActoresService) {
     }
+
 }
