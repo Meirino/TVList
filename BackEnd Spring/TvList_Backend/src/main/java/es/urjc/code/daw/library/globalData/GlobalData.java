8f74517fe52b7c60f31c24ab37d729e5d0b24a08
david <drimer1994@gmail.com>
2016-05-23 17:55:41 +0200

    Commit a GlobalData

---
+++ BackEnd Spring/TvList_Backend/src/main/java/es/urjc/code/daw/library/globalData/GlobalData.java
@@ -0,0 +1,61 @@
+package es.urjc.code.daw.library.globalData;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+/**
+ * This is the entity to store in database user information. It contains the
+ * following basic information:
+ * <ul>
+ * <li>name: The name of the user. This name have to be used to logIn into the
+ * service</li>
+ * <li>passwordHash: The hash of the password. The password in never stored in
+ * plain text to avoid information leak</li>
+ * <li>roles: The roles of this user</li>
+ * 
+ * To check if a user can be logged into the service, this object is loaded from
+ * database and password is verified. If user is authenticated, then this
+ * database object is returned to the user.
+ * 
+ * NOTE: This class is intended to be extended by developer adding new
+ * attributes. Current attributes can not be removed because they are used in
+ * authentication procedures.
+ */
+
+@Entity
+public class GlobalData {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private Long avatarFileIndex;
+
+	public Long getavatarFileIndexAndIncrement(){
+		avatarFileIndex++;
+		return avatarFileIndex;
+	}
+
+	public GlobalData() {
+		avatarFileIndex=(long) 0;
+	}
+
+
+
+
+}
